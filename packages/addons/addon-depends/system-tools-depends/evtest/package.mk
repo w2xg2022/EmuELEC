375d23f08d0595c076fef3181a1bfda439f36d28
w2xg2022 <w2xg2022@users.noreply.github.com>
2026-06-26 22:57:11 +0800

    修复evtest下载源(repo.or.cz失效)，改用gitlab.freedesktop.org的git鏡像並切換為git handler

--- packages/addons/addon-depends/system-tools-depends/evtest/package.mk
+++ packages/addons/addon-depends/system-tools-depends/evtest/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="evtest"
-PKG_VERSION="1.35"
+PKG_VERSION="7580b4477c296274025cd6fb2a6345a7732be6eb"
 PKG_SHA256="a224abeb783561193660999b0c986216d691cb271b5ec3af88c6bb37b55ff51c"
 PKG_LICENSE="GPL"
 PKG_SITE="http://cgit.freedesktop.org/evtest/"
-PKG_URL="https://repo.or.cz/evtest.git/snapshot/evtest-${PKG_VERSION}.tar.gz"
+PKG_URL="https://gitlab.freedesktop.org/libevdev/evtest.git"
 PKG_DEPENDS_TARGET="toolchain libxml2"
 PKG_LONGDESC="A simple tool for input event debugging."
 PKG_TOOLCHAIN="autotools"
