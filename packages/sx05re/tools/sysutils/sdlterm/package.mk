35de128d0d27e56bb3eb174cb68bae71e1fbb1db
w2xg2022 <w2xg2022@users.noreply.github.com>
2026-06-29 12:27:38 +0800

    修复sdlterm裸用主机sdl2-config(非交叉编译sysroot版本)导致/usr/include混入编译参数,触发cc1plus internal compiler error崩溃;改用SYSROOT_PREFIX下的sdl2-config(仿sundog/jzintv做法)

--- packages/sx05re/tools/sysutils/sdlterm/package.mk
+++ packages/sx05re/tools/sysutils/sdlterm/package.mk
@@ -9,7 +9,10 @@ PKG_SHORTDESC="simple SDL2 program to read output of bash scripts"
 PKG_TOOLCHAIN="manual"
 
 make_target() {
-    ${CXX} sdlterm.cpp -o sdlterm `sdl2-config --cflags --libs` -lSDL2_ttf -pthread
+  # NOTE(w2xg2022): 原本裸用sdl2-config會抓到主機系統的(不是交叉編譯sysroot裡的)，
+  # 把/usr/include塞進編譯參數，直接讓cc1plus internal compiler error崩潰。
+  # 仿sundog/jzintv等套件的作法，明確指向SYSROOT_PREFIX裡的sdl2-config。
+    ${CXX} sdlterm.cpp -o sdlterm `${SYSROOT_PREFIX}/usr/bin/sdl2-config --cflags --libs` -lSDL2_ttf -pthread
 }
 
 makeinstall_target() {
