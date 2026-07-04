06b6aefea2e905f7146fc1878ffb96e9bfd60fe9
w2xg2022 <w2xg2022@users.noreply.github.com>
2026-07-04 11:07:48 +0800

    fix(libmodplug): GitLab需登入无法访问，改镜像至repo内
    GitLab上的solarus-games/libmodplug现在需要登入才能存取(git clone/网页都被挡)。这个套件是SDL2_mixer和solarus的必要依赖，把验证过的相同commit内容(533KB)镜像进repo。
    
    同时停用duckstation-lr和viceSA(含其host依赖xa)：确认默认没有使用这两个模拟器，上游源都已失效且找不到可靠替代，改名package.mk为.disabled让建置系统跳过，不影响其他套件。

--- packages/sx05re/tools/sound/modplug/package.mk
+++ packages/sx05re/tools/sound/modplug/package.mk
@@ -3,10 +3,13 @@
 
 PKG_NAME="libmodplug"
 PKG_VERSION="a35e253001b1bc9046b7955d0871f841f88f993b"
+PKG_SHA256="ca51a07b9f6a5f9485c4ab8eb84c96e84a6cea8d3af4f9ba38f8150b8689af5e"
 PKG_ARCH="any"
 PKG_LICENSE="public domain"
 PKG_SITE="https://gitlab.com/solarus-games/libmodplug"
-PKG_URL="${PKG_SITE}.git"
+# NOTE(w2xg2022): GitLab那個repo現在需要登入才能存取(git clone/瀏覽器都被擋，
+# 多次實測確認)。這個套件是SDL2_mixer和solarus的必要依賴，掛掉會拖累一大片
+# 後面的套件。把內容(跟GitLab上要的commit內容一致，已驗證)打包鏡像進repo。
+PKG_URL="https://raw.githubusercontent.com/w2xg2022/EmuELEC/main/packages/sx05re/tools/sound/modplug/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SHORTDESC="Modplug Plugin for XMMS v2.0 / libmodplug v0.8.5"
-GET_HANDLER_SUPPORT="git"
