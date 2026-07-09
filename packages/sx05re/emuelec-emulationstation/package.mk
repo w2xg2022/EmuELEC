ae622bb0c314564b330d4b2118de78e80b6d801c
w2xg2022 <w2xg2022@users.noreply.github.com>
2026-07-09 19:54:44 +0800

    chore(es): 指向es4all 586175c(X/Y显示对称+两开关翻译)

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -6,7 +6,7 @@
 # 布局侦测基础上又拆了游戏内AB/XY互换为两颗独立开关、补齐布局侦测精灵简繁翻译、
 # 摇杆按下文案统一)，而非 es4all/dist/emuelec 里 pin 的旧值。
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="4c69b7bc094bd1a7187c24a3891e8b8540d7c7e5"
+PKG_VERSION="586175cd7683b0105322efc7b5a203ef5cd18bd9"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_REV="1"
 PKG_ARCH="any"
