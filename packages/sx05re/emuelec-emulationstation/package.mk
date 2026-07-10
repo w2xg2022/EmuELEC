38963163ba2d24db99fd7026ade5c1d01e0812ed
w2xg2022 <w2xg2022@users.noreply.github.com>
2026-07-10 14:52:33 +0800

    chore(es): 指向es4all 2ed2356(手柄三层架构-移除AB/XY开关+精灵只问A)

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -6,7 +6,7 @@
 # 布局侦测基础上又拆了游戏内AB/XY互换为两颗独立开关、补齐布局侦测精灵简繁翻译、
 # 摇杆按下文案统一)，而非 es4all/dist/emuelec 里 pin 的旧值。
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="586175cd7683b0105322efc7b5a203ef5cd18bd9"
+PKG_VERSION="2ed235676d75d2c16a928bb4bc45ec81f498f331"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_REV="1"
 PKG_ARCH="any"
